b1d401fcc8931a548ae06e692f0c9a74a43ecd66
Dylan Morgan <morgdyl@gmail.com>
2018-11-24 10:50:31 -0500

    Original commit of using beeps as tuned notes

---
+++ Notes.cs
@@ -0,0 +1,29 @@
+using System;
+namespace testFolder
+{
+    public class notes
+    {
+        public enum note : int {
+            A = 1,
+            BFlat = 2,
+            B = 3,
+            CFlat = B,
+            C = 4,
+            CSharp = 5,
+            DFlat = CSharp,
+            D  = 6, 
+            DSharp = 7,
+            EFlat = DSharp,
+            E = 8,
+            Esharp = 9,
+            F = Esharp,
+            FSharp = 10, 
+            GFlat = FSharp,
+            G = 11,
+            GSharp = 12,
+            AFlat = GSharp                   
+        };
+        
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace testFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lastNote = notes.note.A;
+           while(true)
+           {
+               lastNote = GenerateNextNote(lastNote);
+
+           }
+        }
+        public static notes.note GenerateNextNote(notes.note lastNote)
+        {
+            lastNote = ++lastNote;
+            playNote(lastNote);
+            return lastNote;
+        }
+        //Change teste
+        public static void playNote(notes.note currentNote)
+        {
+            Console.Beep((int)(440 * Math.Pow(Math.Pow(2,(double)1/12),(int)currentNote - 1)), 1000);
+        }
+    }
+}
